8f915c4ffdbc361e953fd851d973f9f0f27cfdd7
AndresitoRap <andresfelipecvs@hotmail.com>
2024-03-31 14:49:02 -0500

    Update time and score

--- src/api/tiempo/controllers/tiempo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tiempo controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::tiempo.tiempo');
--- src/api/tiempo/services/tiempo.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tiempo service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::tiempo.tiempo');
